6ba12198d45737efddbb5ddb159741bc8026e575
KatyaBel <ekaterina.belyaeva21@mail.ru>
2025-02-14 00:35:06 +0500

    выполнено задание урока 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Список";
+const screens = [1, 2, 3];
+const screenPrice = 2400;
+const rollback = true;
+const fullPrice = 3500.5;
+const adaptive = false;
+
+alert('Hello everyone');
+
+console.log('Text in console');
